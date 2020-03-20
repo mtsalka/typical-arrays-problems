93071789bd18e4d9e89cdbdf234d25d78f9bcd25
mtsalka <maryia.miskevich@gmail.com>
2020-03-20 16:44:56 +0100

    feat: task solution for typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+'use strict'
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length) {
+    return Math.min(...array);
+}
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length) {
+    return Math.max(...array)
+}
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum/array.length;
+}
   return 0;
 }
